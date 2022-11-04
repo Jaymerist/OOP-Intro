ac7184fe9c0fb4346972e4dd75386577c5ff63e6
Jaymerist <76460057+Jaymerist@users.noreply.github.com>
2022-11-04 14:29:05 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Purpose: Demonstrate how to create and use a simple class
+ * Input: n/a
+ * Output: StudentData values
+ * Author: Mihiri Kamiss
+ * Date: November 4, 2022
+ * 
+ * */
+
+namespace OOP_Intro
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                //Create a default instance of StudentData
+                StudentData student1 = new StudentData();
+
+                //Create a non-default instance of StudentData
+                StudentData student2 = new StudentData("Mihiri Kamiss", 98);
+
+                //Display the properties of the objects
+                Console.WriteLine($"Name: {student1.Name}, Grade: {student1.Grade}");
+                Console.WriteLine($"Name: {student2.Name}, Grade: {student2.Grade}");
+
+                //Display the instances/objects
+                Console.WriteLine(student1);
+                Console.WriteLine(student2);
+
+                //Try to write to an object's property
+                student2.Grade = 30;
+                Console.WriteLine(student2);
+
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            } 
+          
+        }//end of Main
+    }
+}
---
+++ StudentData.cs
@@ -0,0 +1,63 @@
+﻿
+namespace OOP_Intro
+{
+    internal class StudentData
+    {
+        //Private member fields 
+        private string _name;
+        private int _grade;
+
+        //public Accessors & Mutators
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if(value.Length > 0)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid");
+                }
+            }
+        }//end of Name
+
+        public int Grade
+        {
+            get { return _grade; }
+            set
+            {
+                if(value >= 0 && value <= 100)
+                {
+                    _grade = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid grade");
+                }
+            }
+        }//end of Grade
+
+        //Constructor(s)
+        public StudentData() //Empty constructor 
+        {
+            {
+                Name = "John Doe";
+                Grade = 25;
+            }
+        }
+
+        public StudentData(string name, int grade)//Greedy constructor
+        {
+            Name = name;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\tGrade: {Grade}";
+        }
+    }
+}
